90bcc7f5ae9408e874b5d4a06f145725a679c872
kingpengin <101848128+kingpengin@users.noreply.github.com>
2022-04-12 23:21:42 +0900

    Add files via upload

---
+++ generate.js
@@ -0,0 +1,70 @@
+//定数
+const $AnimationBlock = document.getElementById('AnimationBlock');
+const $Particle = document.getElementById('particle');
+const $RType = document.getElementById('RType');
+const $MCV = document.getElementById('MCV');
+const $DL = document.getElementById('DL');//このなかのargを変更したい
+
+//変数
+
+/*
+//if文
+//アニメーションブロック
+if ($AnimationBlock[0]) {
+    console.log('全部OFF');
+}
+
+if ($AnimationBlock[1]) {
+    console.log('全部ON')
+}
+
+if ($AnimationBlock[2]) {
+    console.log('詳細設定')
+}
+
+//パーティクル
+if ($Particle[0]) {
+    console.log('全部OFF');
+}
+
+if ($Particle[1]) {
+    console.log('全部ON')
+}
+
+if ($Particle[2]) {
+    console.log('詳細設定')
+}
+
+//リソースの種類
+if ($RType[0]) {
+    console.log('グローバルリソース');
+}
+
+if ($RType[1]) {
+    console.log('ワールド');
+}
+
+if ($RType[2]) {
+    console.log('どちらでも使用可能');
+}
+
+if ($RType[3]) {
+    console.log('カスタム');
+}
+
+//マイクラのバージョン
+if ($MCV[0]) {
+    console.log('1.18.12');
+}
+
+if ($MCV[1]) {
+    console.log('1.18.2');
+}
+
+if ($MCV[2]) {
+    console.log('1.18.1');
+}
+
+if ($MCV[3]) {
+    console.log('1.18.0');
+}*/
